57c87c0ceb4b0e63b45bea558c2a07276bd42606
Joseph-Rhodes <josephlrhodes2021@gmail.com>
2025-02-11 10:13:12 -0500

    Initial Commit

---
+++ comicExercise/comicExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  comicExerciseApp.swift
+//  comicExercise
+//
+//  Created by Joseph Rhodes on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct comicExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ comicExerciseTests/comicExerciseTests.swift
@@ -0,0 +1,17 @@
+//
+//  comicExerciseTests.swift
+//  comicExerciseTests
+//
+//  Created by Joseph Rhodes on 2/11/25.
+//
+
+import Testing
+@testable import comicExercise
+
+struct comicExerciseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
